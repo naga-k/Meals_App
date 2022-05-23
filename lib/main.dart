cee45bcc5950f24363c5be3ecc326c240e302763
Naga Karumuri <anjaneyulun02@gmail.com>
2022-05-23 18:59:41 +0530

    bottom navigation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
+import 'package:meals_app/screens/tabs_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
         )
       ),
       routes: {
-        '/' : (ctx) => const CategoriesScreen(),
+        '/' : (ctx) => const TabsScreen(),
         CategoryMealsScreen.routeName : (ctx) => const CategoryMealsScreen(),
         MealDetailScreen.routeName : (ctx) => const MealDetailScreen()
       },
